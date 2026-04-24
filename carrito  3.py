d30692fd61dbb81c51ce049fa4188eb9d5073df7
Franyelis1 <161973655+Franyelis1@users.noreply.github.com>
2026-04-24 03:07:45 +0200

    Add files via upload

---
+++ carrito  3.py
@@ -0,0 +1,45 @@
+carrito = []
+precios = []
+total = []
+
+print("--- Tienda ---")
+print("Producto: 'total' para pagar, 'ver' para revisar, 'salir' para cancelar.")
+
+while True:
+    producto = input("Producto: ").lower()
+
+    if producto == 'salir':
+        print("Compra cancelada")
+        break
+    
+    elif producto == 'ver':
+        if len(carrito) == 0:
+            print("El carrito está vacío.")
+        else:
+            print(f"Tu carrito actual: {carrito}")
+            
+    elif producto == 'total':
+        
+        total = sum(precios)
+        print("--- TICKET  ---")
+        
+        indice = 0
+        while indice < len(carrito):
+            print(f"- {carrito[indice]}: ${precios[indice]}")
+            indice += 1
+            
+        print(f"TOTAL: ${total}")
+        print("Gracias por su compraa")
+        break
+    
+    else:
+       
+        precio_texto = input(f"Precio de {producto}: ")
+        
+       
+        precio_numero = float(precio_texto)
+        
+    
+        carrito.append(producto)
+        precios.append(precio_numero)
+        print(f" Agregado: {producto}")
